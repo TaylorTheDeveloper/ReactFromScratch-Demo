859e5e3ccb7c24bf83afbd11b3bde9de0bb100c2
Taylor Brockhoeft <taylorbrockhoeft@outlook.com>
2021-10-28 08:51:18 -0700

    adds webpack config and webpack tools

---
+++ src/client/webpack.config.client.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: './App.js',
+    output: {
+        filename: 'client-bundle.js',
+        path: path.join(__dirname, '../../dist')
+    },
+    module: {
+        rules: [
+            {
+             loader: 'babel-loader',
+             test: /\.js$/,
+             exclude: /node_modules/   
+            }
+        ]
+    },
+    devServer:{
+        static: path.join(__dirname, '../../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "./public/index.html"),
+
+        }),
+    ],
+}
